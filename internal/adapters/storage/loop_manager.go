65aec5a6ce34dbb5d772a848c07b4cc4fd71e1a0
MiltonJ23 <zingui.mike@ictuniversity.edu.cm>
2025-11-24 05:51:04 +0000

    Improving the CreateVolume method and ensuring it works properly

--- internal/adapters/storage/loop_manager.go
+++ internal/adapters/storage/loop_manager.go
@@ -14,7 +14,7 @@ type LoopStorageManager struct {
 }
 
 func NewLoopStorageManager() ports.StorageManager {
-	path := "/var/lin/nexus/volumes"
+	path := "/var/lib/nexus/volumes"
 	FolderCreationerr := os.MkdirAll(path, 0755)
 	if FolderCreationerr != nil {
 		return nil
@@ -22,9 +22,9 @@ func NewLoopStorageManager() ports.StorageManager {
 	return &LoopStorageManager{BasePath: path}
 }
 
-func (s *LoopStorageManager) CreateVolume(NodeID string, size string) (string, error) {
+/*func (s *LoopStorageManager) CreateVolume(NodeID string, size string) (string, error) {
 	// let's build the volume path, which means including the image file absolute
-	volumePath := filepath.Join(s.BasePath, fmt.Sprintf("%s.img"), NodeID)
+	volumePath := filepath.Join(s.BasePath, fmt.Sprintf("%s.img", NodeID))
 
 	fmt.Printf("Creating the new volume for %s (%s)....", NodeID, size)
 
@@ -43,11 +43,32 @@ func (s *LoopStorageManager) CreateVolume(NodeID string, size string) (string, e
 		return "", fmt.Errorf("failed to format %s to ext4 format: %v", string(output), err.Error())
 	}
 	return volumePath, nil
+}*/
+
+func (s *LoopStorageManager) CreateVolume(NodeID string, size string) (string, error) {
+	// Utilisation de filepath.Join pour éviter les erreurs de slash
+	volumePath := filepath.Join(s.BasePath, fmt.Sprintf("%s.img", NodeID))
+
+	fmt.Printf("Creating the new volume for %s (%s)....", NodeID, size)
+
+	cmd := exec.Command("truncate", "-s", size, volumePath)
+	cmdOutput, cmdError := cmd.CombinedOutput()
+	if cmdError != nil {
+		return "", fmt.Errorf("failed to truncate %s : %v", string(cmdOutput), cmdError.Error())
+	}
+
+	fmt.Printf("  -> now let's format the volume in ext4...\n")
+	cmdFormat := exec.Command("mkfs.ext4", "-F", volumePath)
+	output, err := cmdFormat.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("failed to format %s to ext4 format: %v", string(output), err.Error())
+	}
+	return volumePath, nil
 }
 
 func (s *LoopStorageManager) AttachVolume(VolumePath string) (string, error) {
 	// Let's mount the volume on the node | we will use losetup -f: in charge of finding the first available loop device | --show flag: to display the name of the loop | - P: it is supposed to scan the partitions , a good practice as they say
-	cmd := exec.Command("losetup -f", "--show", "-P", VolumePath)
+	cmd := exec.Command("losetup", "-f", "--show", "-P", VolumePath)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return "", fmt.Errorf("failed losetup %s : %v ", string(output), err.Error())
